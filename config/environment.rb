ff2abcd91939d3d8d35f200988fc93ee174a4423
Jared Shelby <jared.shelby@yale.edu>
2022-06-23 11:00:44 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,9 @@ ActiveRecord::Base.establish_connection(
 
 require './app'
 require_all 'models'
+
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby")
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
+Owner.create(:name => "Sophie")
+Owner.create(:name => "Ann")
